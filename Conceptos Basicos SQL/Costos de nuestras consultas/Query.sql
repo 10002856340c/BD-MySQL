ccd09907db74b9e7c893386f5f32f24864751e4e
Cristian Galeano <101605501+10002856340c@users.noreply.github.com>
2023-01-03 11:50:13 -0500

    Costos de nuestras Querys

---
+++ Conceptos Basicos SQL/Costos de nuestras consultas/Query.sql
@@ -0,0 +1,23 @@
+USE jugos_ventas;
+
+-- Costo "3.75"
+
+SELECT A.CODIGO_DEL_PRODUCTO FROM tabla_de_productos A;
+
+-- Costo "6065.92" 
+-- A medida que nuestras consultas (querys) tenga INNER JOIN LEFT JOIN , GROUP BY , UNION , UNION ALL claramente va incrementar el costo y el tiempo de ejecucion
+
+SELECT A.NOMBRE_DEL_PRODUCTO, B.PRECIO AS TOTAL FROM tabla_de_productos A 
+INNER JOIN items_facturas B 
+ON A.CODIGO_DEL_PRODUCTO = B.CODIGO_DEL_PRODUCTO;
+
+-- Costo 211939.80
+
+SELECT A.NOMBRE_DEL_PRODUCTO, ROUND(SUM(B.PRECIO*B.CANTIDAD),2) AS TOTAL FROM tabla_de_productos A 
+INNER JOIN items_facturas B 
+ON A.CODIGO_DEL_PRODUCTO = B.CODIGO_DEL_PRODUCTO
+INNER JOIN facturas C
+ON B.NUMERO = C.NUMERO
+GROUP BY A.NOMBRE_DEL_PRODUCTO
+
+
