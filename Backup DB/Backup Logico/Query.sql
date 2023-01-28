b9d0b6a9eb53055e7109bcac2bfabf392843c8d9
Cristian Galeano <101605501+10002856340c@users.noreply.github.com>
2023-01-28 17:07:03 -0500

    Update Query.sql

--- Backup DB/Backup Logico/Query.sql
+++ Backup DB/Backup Logico/Query.sql
@@ -12,3 +12,14 @@ LOCK instance for backup;
 -- 4) Despues de hacer nuestro Backup("Copia de seguridad") hacemos nuestro levantamiento de la base datos para que siga funcionado correctamente con el siguiente comando 
 
 UNLOCK INSTANCE;
+
+******************************************************************************************************************************************************************************
+
+-- tambien se puede hacer por la linea de comandos con el siguiente comando :
+
+ *) mysqldump -u [username] -p[password] [database_name] > [backup_file.sql]
+
+username : es el nombre de usuario de MySQL.
+password : es la contraseña de MySQL.
+database_name : es el nombre de la base de datos que deseas hacer una copia de seguridad.
+backup_file.sql : es el nombre del archivo de copia de seguridad que se creará.
